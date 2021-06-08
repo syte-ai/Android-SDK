c4610feec423580674019de252d7a48f5fdf149f
mmarko <mmarko@cloverdynamics.com>
2021-06-08 16:40:02 +0300

    SDK-21: Logger

---
+++ android-sdk/src/main/java/com/syte/ai/android_sdk/util/SyteLogger.java
@@ -0,0 +1,53 @@
+package com.syte.ai.android_sdk.util;
+
+import android.util.Log;
+
+public class SyteLogger {
+
+    public enum LogLevel {
+        VERBOSE, INFO, DEBUG, WARN, ERROR;
+    }
+
+    private static LogLevel mLogLevel = LogLevel.ERROR;
+
+    private static String appendTag(String TAG) {
+        return ":SYTE:" + TAG;
+    }
+
+    public static void setLogLevel(LogLevel logLevel) {
+        mLogLevel = logLevel;
+    }
+
+    public static void v(String TAG, String message) {
+        if (mLogLevel.ordinal() > LogLevel.VERBOSE.ordinal()) {
+            return;
+        }
+        Log.i(appendTag(TAG), message);
+    }
+
+    public static void i(String TAG, String message) {
+        if (mLogLevel.ordinal() > LogLevel.INFO.ordinal()) {
+            return;
+        }
+        Log.i(appendTag(TAG), message);
+    }
+
+    public static void d(String TAG, String message) {
+        if (mLogLevel.ordinal() > LogLevel.DEBUG.ordinal()) {
+            return;
+        }
+        Log.i(appendTag(TAG), message);
+    }
+
+    public static void w(String TAG, String message) {
+        if (mLogLevel.ordinal() > LogLevel.WARN.ordinal()) {
+            return;
+        }
+        Log.i(appendTag(TAG), message);
+    }
+
+    public static void e(String TAG, String message) {
+        Log.i(appendTag(TAG), message);
+    }
+
+}
