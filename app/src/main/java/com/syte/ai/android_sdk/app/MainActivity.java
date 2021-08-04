d7972b9227d669648613a9fb022e7822b014e844
mmarko <mmarko@cloverdynamics.com>
2021-08-04 15:18:09 +0300

    show version at the bottom

--- app/src/main/java/com/syte/ai/android_sdk/app/MainActivity.java
+++ app/src/main/java/com/syte/ai/android_sdk/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.syte.ai.android_sdk.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.syte.ai.android_sdk.SyteCallback;
 import com.syte.ai.android_sdk.data.result.SyteResult;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.version)).setText(BuildConfig.VERSION_NAME);
         mSyteManager = SDKApplication.getInstance().getSyteManager();
         mSyteManager.initialize(syteResult -> {
             if (syteResult.isSuccessful) {
